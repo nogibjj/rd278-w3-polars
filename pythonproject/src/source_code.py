63e21a8313be2ebd2ecf64ffd9fdf694f72900e1
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-12 22:43:39 -0400

    Update source_code.py

--- pythonproject/src/source_code.py
+++ pythonproject/src/source_code.py
@@ -4,6 +4,7 @@ This module takes a csv and returns statistics about it
 import matplotlib.pyplot as plt
 import polars as pl
 
+
 def descriptive_statistics(data):
     """
     Calculate descriptive statistics for a DataFrame.
@@ -17,40 +18,40 @@ def descriptive_statistics(data):
     """
     return data.describe()
 
-def generating_plot(data,x_variable,y_variable,title,size=None):
-    '''
+
+def generating_plot(data, x_variable, y_variable, title, size=None):
+    """
     Parameters:
         data: Polar DataFrame
         x_variable: String of the variable in axis X from data
         y_variable: String of the variable in axis Y from data
-        size: String if a variable is required to 
+        size: String if a variable is required to
         title: String for the plot title
-    '''
-    
-    x=data[x_variable]
-    y=data[y_variable]
+    """
     if size is None:
-        area=1
+        area = 1
     else:
-        area = data[size]*10/data[size].mean()
-    #Labeling
-    plt.scatter(x, y, s=area, alpha=0.5)
+        area = data[size] * 10 / data[size].mean()
+    # Labeling
+    plt.scatter(data[x_variable], data[y_variable], s=area, alpha=0.5)
     plt.xlabel(x_variable)
     plt.ylabel(y_variable)
     plt.title(title)
     plt.show()
-    plt.savefig(title+'.png', dpi=300, bbox_inches='tight')
+    plt.savefig(title + ".png", dpi=300, bbox_inches="tight")
 
 
 if __name__ == "__main__":
-
-    data_pl=pl.read_csv("pythonproject/src/data/median-income-by-country-2023.csv")
+    data_pl = pl.read_csv("pythonproject/src/data/median-income-by-country-2023.csv")
 
     print(type(data_pl))
 
     print(type(descriptive_statistics(data_pl)))
 
-    generating_plot(data_pl,x_variable="gdpPerCapitaPPP",
-                    y_variable="meanIncome",
-                    size="pop2023",
-                    title="GDP per Capita vs Avg income (size proportional Population)")
+    generating_plot(
+        data_pl,
+        x_variable="gdpPerCapitaPPP",
+        y_variable="meanIncome",
+        size="pop2023",
+        title="GDP per Capita vs Avg income (size proportional Population)",
+    )
